4f079174f3e82b6700099e732df835c3d6e2434e
Mounika1494 <moed4346@colorado.edu>
2020-05-04 17:19:24 -0600

    some exception

--- Karatsuba/karatsuba.c
+++ Karatsuba/karatsuba.c
@@ -4,10 +4,10 @@ int Multiply(int m,int n);
 
 int findnoofdigits(int k)
 {
-int no_of_digits = 1;
+int no_of_digits = 0;
 while(k)
 {
-k = k>>4;
+k = k/10;
 no_of_digits++;
 }
 printf("no_of_digits:%d\n",no_of_digits);
@@ -24,7 +24,7 @@ n--;
 return m;
 }
 
-void karatsuba(int a,int b,int c,int d,int n)
+int karatsuba(int a,int b,int c,int d,int n)
 {
  printf("a:%d,b:%d,c:%d,d=%d,n=%d\n",a,b,c,d,n);
  int p1 = Multiply(a,c);
@@ -33,29 +33,21 @@ void karatsuba(int a,int b,int c,int d,int n)
  printf("p1:%d,p2:%d,p3:%d",p1,p2,p3);
  int result = power(10,n)*p1 + power(10,n/2)*(p2-p1-p3)+ p3;
  printf("result:%d\n",result);
+ return result;
 }
 
 int Multiply(int m,int n)
 	{
 	 int no_of_digits;
-         int mask = 0xF;
   	 no_of_digits = findnoofdigits(m);
          int shift = no_of_digits/2;
-         int dup_shift = shift;
          if(no_of_digits == 1)
          return (m*n);
- 	 while(dup_shift-1)
- 	 {
-           mask = (mask<<4) | mask;
-	   dup_shift--;
-         }
-         printf("mask:%0x\n",mask);
-         printf("%d\n",m&mask);
-         printf("%d\n",m>>(shift*4));
- 	 
-         printf("%d\n",n&mask);
-         printf("%d\n",n>>(shift*4));
-         //karatsuba(m&mask,m>>(shift*4),n&mask,n>>(shift*4),no_of_digits);
+ 	 int a = m/power(10,shift);
+	 int b = m - (a* power(10,shift));
+	 int c = n/power(10,shift);
+	 int d = n - (c* power(10,shift));
+         return karatsuba(a,b,c,d,no_of_digits);
 	}
 
 int main(void)
