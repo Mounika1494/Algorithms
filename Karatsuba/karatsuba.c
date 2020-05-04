9caa14ba15bf83381c94ef70d94b09f17f051596
Mounika1494 <moed4346@colorado.edu>
2020-05-04 17:59:40 -0600

    processor bit

--- Karatsuba/karatsuba.c
+++ Karatsuba/karatsuba.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
+#include<stdint.h>
 
 int Multiply(int m,int n);
 
 int findnoofdigits(int k)
 {
 int no_of_digits = 0;
+if(k== 0)
+return 1;
 while(k)
 {
 k = k/10;
@@ -30,15 +33,20 @@ int karatsuba(int a,int b,int c,int d,int n)
  int p1 = Multiply(a,c);
  int p2 = Multiply((a+b),(c+d));
  int p3 = Multiply(b,d);
- printf("p1:%d,p2:%d,p3:%d",p1,p2,p3);
- int result = power(10,n)*p1 + power(10,n/2)*(p2-p1-p3)+ p3;
- printf("result:%d\n",result);
+ int p4 = p2-p1-p3;
+ printf("p1:%d,p2:%d,p3:%d,p4:%d n:%d\n",p1,p2,p3,p4,n);
+ uint64_t result1 = power(10,n)*p1;
+ int result2 = power(10,n/2)*p4;
+ int result3 = p3;
+ printf("r1:%d,r2:%d,r3:%d\n",result1,result2,result3);
+ uint64_t result = result1+result2+result3;
+ printf("result:%u\n",result);
  return result;
 }
 
 int Multiply(int m,int n)
 	{
-	 printf(m:%d,n:%d\n",m,n);
+	 printf("m:%d,n:%d\n",m,n);
 	 int no_of_digits;
   	 no_of_digits = findnoofdigits(m);
          int shift = no_of_digits/2;
